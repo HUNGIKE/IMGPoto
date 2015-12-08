39e8e15af4b6357bf4e9e029236dd36d33064efb
ben_ke <ben_ke@trend.com.tw>
2015-12-08 23:03:35 +0800

    added App.java

--- src/main/java/HIK/App.java
+++ src/main/java/HIK/App.java
@@ -14,54 +14,27 @@ import javax.imageio.ImageIO;
  */
 public class App {
     public static void main( String[] args ) throws IOException {
-    	File input = new File("img.jpg");
-    	BufferedImage image = ImageIO.read(input)
-    			,image2=new BufferedImage(image.getWidth(),image.getHeight(),image.getType());
+    	String imgName="img.jpg";
+    	
+    	
+    	File input = new File(imgName);
+    	BufferedImage image = ImageIO.read(input),
+    			image2=new BufferedImage(image.getWidth(),image.getHeight(),image.getType());
     	int w=image.getWidth(),h=image.getHeight();
-    	for(int i=0;i<w;i++){
-    		for(int j=0;j<h;j++){
-    			
-    			//process1(image,image2,i,j);
-    			
-    			try{
-    			int u=image.getRGB(i-1,j),
-    				d=image.getRGB(i+1,j),
-    				r=image.getRGB(i,j-1),
-    				l=image.getRGB(i,j+1),
-    				u_l=image.getRGB(i-1,j+1),
-    		    	d_l=image.getRGB(i+1,j+1),
-    		    	u_r=image.getRGB(i-1,j-1),
-    		    	d_r=image.getRGB(i+1,j-1),
-    				t=image.getRGB(i,j);
-    			int A=90;
-    			if(    Math.abs(t-r)>256*256*A ||
-    			       Math.abs(t-l)>256*256*A ||
-    			       Math.abs(t-u)>256*256*A ||
-    			       Math.abs(t-d)>256*256*A ||
-       			       Math.abs(t-u_l)>256*256*A ||
-    			       Math.abs(t-u_r)>256*256*A ||
-    			       Math.abs(t-d_l)>256*256*A ||
-    			       Math.abs(t-d_r)>256*256*A || 
-    				Math.abs(image.getRGB(i,j)-Color.red.getRGB()) < 256*256*A 
-    					){
-    				// image2.setRGB(i,j,Color.black.getRGB());
-    				image2.setRGB(i,j,image.getRGB(i,j));
-    				//process1(image,image2,i,j);;
-    			}else{
-
-    				//image2.setRGB(i,j,Color.white.getRGB());
-    				image2.setRGB(i,j,image.getRGB(i,j));
-    				//process1(image,image2,i,j);;
-    			
-    			}
-    			}catch(Throwable t){}
-    			
-    			
-    			
+    	int [][] a=new int[][]{
+    			{-1,-1,-1},
+    			{-1,8,-1},
+    			{-1,-1,-1}
+    	};
+    	
+    	int r=Color.RED.getRed();
+    	System.out.println(r);
+    	for(int x=0;x<w;x++){
+    		for(int y=0;y<h;y++){Convolution(image,image2,a,x,y);
     		}
     	}
     	
-    	FileOutputStream fos=new FileOutputStream("img2.jpg");
+    	FileOutputStream fos=new FileOutputStream("output_red_"+imgName);
     	ImageIO.write(image2,"jpg", fos);
     	fos.close();
     	
@@ -75,7 +48,58 @@ public class App {
     }
     
     
-    public static int  getRGB(BufferedImage image,int x,int y){
+    public static void Convolution(BufferedImage img,BufferedImage img2,int[][] a,int x,int y){
+    	
+    	
+    	
+    	
+    	float r=0;
+    	r+=(a[0][0]*new Color(getRGB(img,x-1, y-1)).getRed());
+    	r+=(a[0][1]*new Color(getRGB(img,x-1, y)).getRed());
+    	r+=(a[0][2]*new Color(getRGB(img,x-1, y+1)).getRed());
+    	r+=(a[1][0]*new Color(getRGB(img,x, y-1)).getRed());
+    	r+=(a[1][1]*new Color(getRGB(img,x, y)).getRed());
+    	r+=(a[1][2]*new Color(getRGB(img,x, y+1)).getRed());
+    	r+=(a[2][0]*new Color(getRGB(img,x+1, y-1)).getRed());
+    	r+=(a[2][1]*new Color(getRGB(img,x+1, y)).getRed());
+    	r+=(a[2][2]*new Color(getRGB(img,x+1, y+1)).getRed());
+    	
+
+    	float g=0;
+    	g+=(a[0][0]*new Color(getRGB(img,x-1, y-1)).getGreen());
+    	g+=(a[0][1]*new Color(getRGB(img,x-1, y)).getGreen());
+    	g+=(a[0][2]*new Color(getRGB(img,x-1, y+1)).getGreen());
+    	g+=(a[1][0]*new Color(getRGB(img,x, y-1)).getGreen());
+    	g+=(a[1][1]*new Color(getRGB(img,x, y)).getGreen());
+    	g+=(a[1][2]*new Color(getRGB(img,x, y+1)).getGreen());
+    	g+=(a[2][0]*new Color(getRGB(img,x+1, y-1)).getGreen());
+    	g+=(a[2][1]*new Color(getRGB(img,x+1, y)).getGreen());
+    	g+=(a[2][2]*new Color(getRGB(img,x+1, y+1)).getGreen());
+    	
+    	
+    	float b=0;
+    	b+=(a[0][0]*new Color(getRGB(img,x-1, y-1)).getBlue());
+    	b+=(a[0][1]*new Color(getRGB(img,x-1, y)).getBlue());
+    	b+=(a[0][2]*new Color(getRGB(img,x-1, y+1)).getBlue());
+    	b+=(a[1][0]*new Color(getRGB(img,x, y-1)).getBlue());
+    	b+=(a[1][1]*new Color(getRGB(img,x, y)).getBlue());
+    	b+=(a[1][2]*new Color(getRGB(img,x, y+1)).getBlue());
+    	b+=(a[2][0]*new Color(getRGB(img,x+1, y-1)).getBlue());
+    	b+=(a[2][1]*new Color(getRGB(img,x+1, y)).getBlue());
+    	b+=(a[2][2]*new Color(getRGB(img,x+1, y+1)).getBlue());
+    	
+    	float A=1;
+    	//r=(r+2040f)/4080/A; g=(g+2040f)/4080/A; b=(b+2040f)/4080/A;
+    	r=Math.abs(r)/8/255;g=Math.abs(g)/8/255;b=Math.abs(b)/8/255;
+    	//float t=0.14f; r=b=g=((r+g+b)>t)?1:0;
+    	float t=0.14f; r=((r+g+b)>t)?1:0;g=b=0;
+    	
+    	//System.out.println("r="+r+",g="+g+",b="+b+",grey="+(r+g+b));
+    	img2.setRGB(x, y,new Color(r,g,b).getRGB());
+    }
+    
+    
+    public static int getRGB(BufferedImage image,int x,int y){
     	try{
     		return image.getRGB(x, y);
     	}catch(IndexOutOfBoundsException iobe){
